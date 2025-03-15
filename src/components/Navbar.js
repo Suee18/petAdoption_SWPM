ce150067cd8b13dd80b6913043d2426855974b97
Suee18 <121303661+Suee18@users.noreply.github.com>
2025-03-15 02:47:52 +0200

    KAN-1 KAN-4 nav bar matching colors fonts `

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
 // src/components/Navbar.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  // Add scroll detection for background opacity change
+  const [scrolled, setScrolled] = useState(false);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [scrolled]);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${scrolled ? 'navbar-scrolled' : ''}`}>
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
           Cairo Paws
@@ -13,26 +28,24 @@ const Navbar = () => {
         <ul className="nav-menu">
           <li className="nav-item">
             <Link to="/" className="nav-links">
-              Home 
+              Home
             </Link>
           </li>
-         
           <li className="nav-item">
             <Link to="/register-shelter" className="nav-links">
               Register Shelter
             </Link>
-          </li >
-          <li className="nav-item">
-          <Link to="/available-pets" className="nav-links">
-      AvailablePets
-          </Link>
           </li>
           <li className="nav-item">
-            <Link to="/login" className="nav-links">
-            Log in
+            <Link to="/available-pets" className="nav-links">
+              Available Pets
             </Link>
           </li>
-          
+          <li className="nav-item">
+            <Link to="/login" className="nav-links login-link">
+              Log in
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
